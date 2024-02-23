91ac552e2be259f7cf34fa279042a126f854168a
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2024-02-23 21:27:37 +0000

    mapping da BD

---
+++ GESImoveis/database/migrations/2024_02_23_211600_create_fotos_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fotos', function (Blueprint $table) {
+            $table->increments('id_foto');
+            $table->integer('id_imovel')->unsigned();
+            $table->string('foto', 255);
+            $table->foreign('id_imovel')->references('id_imovel')->on('imovel');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fotos');
+    }
+};
