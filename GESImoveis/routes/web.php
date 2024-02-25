712d94bf483c3c51a436ea1080e5a4d796c64b29
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2024-02-25 16:31:05 +0000

    controllers

--- GESImoveis/routes/web.php
+++ GESImoveis/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProprietarioController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,3 +31,6 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+Route::get('/proprietario/dashboard', [ProprietarioController::class, 'ProprietarioDashboard'])->name('proprietario.dashboard');
