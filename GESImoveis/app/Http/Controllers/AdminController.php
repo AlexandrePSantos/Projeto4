e0297e20e482e8f53b13283b23f17a60290ca1c2
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2024-03-06 22:50:12 +0000

    editar perfil

--- GESImoveis/app/Http/Controllers/AdminController.php
+++ GESImoveis/app/Http/Controllers/AdminController.php
@@ -35,4 +35,62 @@ class AdminController extends Controller
         $profileData = User::find($id);
         return view('admin.admin_profile_view',compact('profileData'));
     }
+
+    public function AdminProfileStore(Request $request)
+    {
+        $id = Auth::user()->id;
+        $data = User::find($id);
+        $data->titulo = $request->titulo;
+        $data->name = $request->name;
+        $data->email = $request->email;
+        $data->telemovel = $request->telemovel;
+        $data->telefone = $request->telefone;
+        $data->nif = $request->nif;
+        $data->pais = $request->pais;
+        $data->cidade = $request->cidade;
+        $data->morada = $request->morada;
+        $data->morada = $request->morada;
+        $data->codigo_postal = $request->codigo_postal;
+
+        if($request->file('foto')){
+            $file = $request->file('foto');
+            @unlink(public_path('upload/admin_images/'.$data->foto));
+            $filename = date('YmdHi').$file->getClientOriginalName();
+            $file->move(public_path('upload/admin_images'),$filename);
+            $data['foto'] = $filename;
+        }
+
+        $data->save();
+
+        $notification = array(
+            'message' => 'Perfil atualizado com sucesso',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+    }
+
+    public function AdminUpdatePassword(Request $request)
+    {
+        $request->validate([
+            'old_password' => 'required',
+            'new_password' => 'required|confirmed'
+        ]);
+
+        if(!Hash::check($request->old_password,Auth::user()->password)){
+            $notification = array(
+                'message' => 'Password antiga incorreta!',
+                'alert-type' => 'error'
+            );
+            return back()->with($notification);
+        }
+
+        User::whereId(auth()->user()->id)->update(['password'=> Hash::make($request->new_password)]);
+            $notification = array(
+                'message' => 'Password atualizada com sucesso!',
+                'alert-type' => 'success'
+            );
+
+        return back()->with($notification);
+    }
 }
--- GESImoveis/resources/views/admin/admin_profile_view.blade.php
+++ GESImoveis/resources/views/admin/admin_profile_view.blade.php
@@ -1,6 +1,8 @@
 @extends('admin.admin_dashboard')
 @section('admin')
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
 <div class="page-content">
     <div class="row profile-body">
       <!-- left wrapper start -->
@@ -39,27 +41,42 @@
             </div>
             <div class="mt-3">
                 <label class="tx-11 fw-bolder mb-0 text-uppercase">Morada:</label>
-                <p class="text-muted">{{ ($profileData->pais) }}</p>
-                <p class="text-muted">{{ ($profileData->cidade) }}</p>
-                <p class="text-muted">{{ ($profileData->morada) }}</p>
-                <p class="text-muted">{{ ($profileData->codigo_postal) }}</p>
+                <p class="text-muted">{{ ($profileData->pais) }}, {{ ($profileData->cidade) }}, {{ ($profileData->morada) }}, {{ ($profileData->codigo_postal) }}</p>
+                <p class="text-muted"></p>
             </div>
-
-            {{-- <div class="mt-3 d-flex social-links">
-              <a href="javascript:;" class="btn btn-icon border btn-xs me-2">
-                <i data-feather="github"></i>
-              </a>
-              <a href="javascript:;" class="btn btn-icon border btn-xs me-2">
-                <i data-feather="twitter"></i>
-              </a>
-              <a href="javascript:;" class="btn btn-icon border btn-xs me-2">
-                <i data-feather="instagram"></i>
-              </a>
-            </div> --}}
           </div>
         </div>
+        <!-- new wrapper start -->
+        <div class="card rounded mt-3">
+            <div class="card-body">
+                <h6 class="card-title">Alterar Password</h6>
+                <form method="POST" action="{{ route('admin.update.password') }}" class="forms-sample" >
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Password Atual</label>
+                        <input type="password" name="old_password" class="form-control @error('old_password') is-invalid @enderror " id="exampleInputEmail1" autocomplete="off" placeholder="*****">
+                        @error('old_password')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Nova Password</label>
+                        <input type="password" name="new_password" class="form-control @error('new_password') is-invalid @enderror " id="exampleInputEmail1" autocomplete="off" placeholder="*****">
+                        @error('new_password')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Confirmar Password</label>
+                        <input type="password" name="conf_password" class="form-control" id="exampleInputEmail1" autocomplete="off" placeholder="*****">
+                    </div>
+                    <button type="submit" class="btn btn-primary me-2">Confirmar Alteração</button>
+                </form>
+            </div>
+        </div>
+        <!-- new wrapper end -->
       </div>
       <!-- left wrapper end -->
+
       <!-- middle wrapper start -->
       <div class="col-md-8 col-xl-8 middle-wrapper">
         <div class="row">
@@ -67,60 +84,63 @@
                 <div class="card-body">
                     <h6 class="card-title">Editar Informação</h6>
 
-                    <form class="forms-sample">
+                    <form method="POST" action="{{ route('admin.profile.store') }}" class="forms-sample" enctype="multipart/form-data">
+                        @csrf
                         <div class="mb-3">
                             <label for="exampleInputTitulo" class="form-label">Titulo</label>
-                            <input type="email" name="titulo" class="form-control" id="exampleInputEmail1" value="{{$profileData->titulo}}">
+                            <select name="titulo" class="form-control" id="exampleInputTitulo">
+                                <option value="Sr." {{ $profileData->titulo == 'Sr.' ? 'selected' : '' }}>Sr.</option>
+                                <option value="Sra." {{ $profileData->titulo == 'Sra.' ? 'selected' : '' }}>Sra.</option>
+                            </select>
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputUsername1" class="form-label">Nome</label>
-                            <input type="text" class="form-control" id="exampleInputUsername1" autocomplete="off" placeholder="Username">
+                            <input type="text" name="name" class="form-control" id="exampleInputUsername1" placeholder="Nome" value="{{$profileData->name}}">
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Email</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                            <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Email" value="{{$profileData->email}}">
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Telemovel</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                            <input type="text" name="telemovel" class="form-control" id="exampleInputEmail1" placeholder="Telemovel" value="{{$profileData->telemovel}}">
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Telefone</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                            <input type="text" name="telefone" class="form-control" id="exampleInputEmail1" placeholder="Telefone" value="{{$profileData->telefone}}">
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">NIF</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">País</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Cidade</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                            <input type="text" name="nif" class="form-control" id="exampleInputEmail1" placeholder="NIF" value="{{$profileData->nif}}">
                         </div>
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Morada</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                            <div class="row">
+                                <div class="col-md-3 col-sm-6 mb-3">
+                                    <input type="text" name="pais" class="form-control" id="exampleInputEmail1" placeholder="País" value="{{$profileData->pais}}">
+                                </div>
+                                <div class="col-md-3 col-sm-6 mb-3">
+                                    <input type="text" name="cidade" class="form-control" id="exampleInputEmail1" placeholder="Cidade" value="{{$profileData->cidade}}">
+                                </div>
+                                <div class="col-md-4 col-sm-6 mb-3">
+                                    <input type="text" name="morada" class="form-control" id="exampleInputEmail1" placeholder="Morada" value="{{$profileData->morada}}">
+                                </div>
+                                <div class="col-md-2 col-sm-6 mb-3">
+                                    <input type="text" name="codigo_postal" class="form-control" id="exampleInputEmail1" placeholder="Código Postal" value="{{$profileData->codigo_postal}}">
+                                </div>
+                            </div>
                         </div>
                         <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Código Postal</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                            <label class="form-label" for="formFile">Foto</label>
+                            <input class="form-control" name="foto" type="file" id="image">
                         </div>
-
                         <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" autocomplete="off" placeholder="Password">
+                            <img id="showImage" class="wd-80 rounded-circle"
+                                src="{{ (!empty($profileData->foto)) ? url('upload/admin_images/'.$profileData->foto) : url('upload/no_image.jpg') }}"
+                                alt="profile"
+                            >
                         </div>
-                        <div class="form-check mb-3">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">
-                                Remember me
-                            </label>
-                        </div>
-                        <button type="submit" class="btn btn-primary me-2">Submit</button>
-                        <button class="btn btn-secondary">Cancel</button>
+                        <button type="submit" class="btn btn-primary me-2">Confirmar Alterações</button>
                     </form>
                 </div>
             </div>
@@ -129,5 +149,17 @@
       <!-- middle wrapper end -->
     </div>
 </div>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#image').change(function(e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#showImage').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+</script>
 
 @endsection
