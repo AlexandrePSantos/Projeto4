b1a773523987f2c9731031b49c3f3fdfc65939c5
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2024-04-09 23:48:43 +0100

    Update header.blade.php

--- GESImoveis/resources/views/admin/body/header.blade.php
+++ GESImoveis/resources/views/admin/body/header.blade.php
@@ -5,46 +5,50 @@
     img.logoimg { max-height: 100px !important; max-width: 100% !important; height: auto !important; width: auto   !important; }
     .nav-gesfaturacao { margin-left: 15px; margin-right: 15px; }
     .nav-gesfaturacao>li>a { padding-right: 10px; padding-left: 10px; }
-    .mts { padding-top: 20px; } /* Add this line */
+    .mts { padding-top: 20px; }
+    .navbar-nav { display: flex; flex-direction: row; }
+    .navbar-nav .nav-item { margin-right: 30px; }
 </style>
-
-<!-- NAVBAR -->
-<div class="col-md-12">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('inquilinos.index') }}">Inquilinos</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('utilizadores.index') }}">Utilizadores</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{ route('imoveis.index') }}">Imoveis</a>
-                </li>
-            </ul>
-        </div>
-    </nav>
-</div>
 
 <!-- HEADER AND STATS -->
 <div class="col-md-12 mts">
     <div class="row">
         <div class="col-md-2 text-center flt-left mtl no-padding">
-            <a href="{{ url('/gesfaturacao') }}">
+            <a href="{{ route('dashboard') }}">
                 <img class="logoimg" src="{{ asset('uploads/faturacao/logo_default.png') }}" alt="Logotipo">
             </a>
         </div>
+
         <div class="col-md-3 mtl">
             @php
                 $id = Auth::user()->id;
                 $profileData = App\Models\User::find($id);
             @endphp
-            <h4>Bem Vindo ao GESImoveis!</h4>
-            <p>Bem-vindo, <a href="{{ route('admin.profile') }}" aria-haspopup="true" aria-expanded="false">{{ $profileData->name }}</a><p>
+            <p>Bem Vindo ao GESImoveis, <a href="{{ route('admin.profile') }}" aria-haspopup="true" aria-expanded="false">{{ $profileData->name }}</a><p>
             <p>
                 <a class="options-link text-red" href="{{ route('admin.logout') }}">Sair</a>
             </p>
+            <!-- NAVBAR -->
+            <div class="col-md-12">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('inquilinos.index') }}">Inquilinos</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('utilizadores.index') }}">Utilizadores</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('imoveis.index') }}">Imoveis</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
         </div>
     </div>
 </div>
+
+
+
