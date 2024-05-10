d74519d1024fecea97af9cd5ff78913fe213ada9
gomesam <gomesam2000@gmail.com>
2024-05-10 16:33:21 +0100

    Update create.blade.php

--- GESImoveis/resources/views/pagamento/create.blade.php
+++ GESImoveis/resources/views/pagamento/create.blade.php
@@ -1,10 +1,30 @@
 @extends('admin.admin_dashboard')
-{{-- @extends('proprietario.proprietario_dashboard') --> por criar --}}
-
 @section('admin')
-<p>Adicionar Inquilino Admin</p>
-@endsection
 
-{{-- @section('proprietario')
-<p>Adicionar Inquilino Prop</p> --> por criar
-@endsection --}}
+<form action="{{ route('pagamento.store') }}" method="POST">
+    @csrf
+
+    <div>
+        <label for="id_contrato">ID Contrato</label>
+        <input type="number" id="id_contrato" name="id_contrato" required>
+    </div>
+
+    <div>
+        <label for="data_pag">Data Pagamento</label>
+        <input type="date" id="data_pag" name="data_pag" required>
+    </div>
+
+    <div>
+        <label for="metodo_pag">Método de Pagamento</label>
+        <input type="text" id="metodo_pag" name="metodo_pag" required>
+    </div>
+
+    <div>
+        <label for="valor">Valor</label>
+        <input type="number" step="0.01" id="valor" name="valor" required>
+    </div>
+
+    <button type="submit">Criar Pagamento</button>
+</form>
+
+@endsection
