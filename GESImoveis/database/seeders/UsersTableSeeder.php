c37f1836290f6fe5bb375025879bca77a935d178
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2024-05-27 15:08:41 +0100

    Update UsersTableSeeder.php

--- GESImoveis/database/seeders/UsersTableSeeder.php
+++ GESImoveis/database/seeders/UsersTableSeeder.php
@@ -15,11 +15,34 @@ class UsersTableSeeder extends Seeder
     public function run(): void
     {
         DB::table('users')->insert([
-            'name' => 'admin',
-            'email' => 'admin@ipvc.pt',
-            'password' => Hash::make('admin'),
-            'role' => 'admin',
-            'estado' => 'ativo',
+            [
+                'name' => 'admin',
+                'email' => 'admin@ipvc.pt',
+                'password' => Hash::make('admin'),
+                'role' => 'admin',
+                'estado' => 'ativo',
+            ],
+            [
+                'name' => 'proprietario1',
+                'email' => 'proprietario1@ipvc.pt',
+                'password' => Hash::make('proprietario1'),
+                'role' => 'proprietario',
+                'estado' => 'ativo',
+            ],
+            [
+                'name' => 'proprietario2',
+                'email' => 'proprietario2@ipvc.pt',
+                'password' => Hash::make('proprietario2'),
+                'role' => 'proprietario',
+                'estado' => 'ativo',
+            ],
+            [
+                'name' => 'admin2',
+                'email' => 'admin2@ipvc.pt',
+                'password' => Hash::make('admin2'),
+                'role' => 'admin',
+                'estado' => 'ativo',
+            ],
         ]);
     }
 }
