a0461039755970ddfd0af5f8e3da8e512dd8bfe9
AlexandrePSantos <69675582+AlexandrePSantos@users.noreply.github.com>
2024-06-20 15:24:30 +0100

    forget the template

--- GESImoveis/resources/views/admin/body/footer.blade.php
+++ GESImoveis/resources/views/admin/body/footer.blade.php
@@ -3,11 +3,43 @@
     <p class="text-muted">Handcrafted With <i class="mb-1 text-primary ms-1 icon-sm" data-feather="heart"></i></p>
 </footer> --}}
 
-<footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between px-4 py-3 border-top small">
+{{-- <footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between px-4 py-3 border-top small">
 	<div class="container">
 		<ul class="list-inline ">
 			<li>
                 <a href="https://www.ftkode.com" target="_blank">FTKode</a>
+			</li>
+		</ul>
+	</div>
+</footer> --}}
+
+<footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between px-4 py-3 border-top small">
+	<div class="container">
+		<ul class="list-inline">
+			<li class="list-inline-item">
+				© GESFaturação v1.0 | AT: Nº 2716
+			</li>
+			<li class="list-inline-item">
+				<a href="/definicoes/dados_acesso">Dados de Acesso</a>
+			</li>
+			<li class="list-inline-item">
+				<a href="/definicoes/formacao/"><i class="fa fa-youtube-play"></i> Formações</a>
+			</li>
+			<li class="list-inline-item logo">
+				<img src="{{ asset('img/ges-faturacao-white.png') }}" alt="GES Faturação">
+			</li>
+		</ul>
+	</div>
+</footer>
+
+<footer class="special_pos_footer">
+	<div class="container">
+		<ul class="list-inline">
+			<li class="list-inline-item">
+				© GESFaturação v1.0
+			</li>
+			<li class="list-inline-item logo">
+				<img src="{{ asset('img/ges-faturacao-white.png') }}" alt="GES Faturação">
 			</li>
 		</ul>
 	</div>
--- GESImoveis/resources/views/auth/login.blade.php
+++ GESImoveis/resources/views/auth/login.blade.php
@@ -3,28 +3,29 @@
 <head>
     <title>GESImóveis | Software de Gestão de Imóveis</title>
     <meta charset="utf-8">
-    <link rel="shortcut icon" type="image/png" href="/images/favicon.png"/>
+    <link rel="shortcut icon" type="image/png" href="{{ asset('images/favicon.png') }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="noindex"/>
 
-    <link rel="stylesheet" href="/css/font-awesome.min.css">
-    <link type="text/css" rel="stylesheet" href="/fonts/opensans.css">
-    <link type="text/css" rel="stylesheet" href="/fonts/oswald.css">
-    <link type="text/css" rel="stylesheet" href="css/jquery-ui-1.10.4.custom.min.css">
-    <link type="text/css" rel="stylesheet" href="css/bootstrap.min.css">
-    <link type="text/css" rel="stylesheet" href="css/sco.message.css">
-    <link type="text/css" rel="stylesheet" href="css/style-responsive.css">
-    <link rel="stylesheet" href="/js/vendors/bootstrap-select/bootstrap-select.min.css">
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('fonts/opensans.css') }}">
+    <link rel="stylesheet" href="{{ asset('fonts/oswald.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/jquery-ui-1.10.4.custom.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/sco.message.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style-responsive.css') }}">
+    <link rel="stylesheet" href="{{ asset('js/vendors/bootstrap-select/bootstrap-select.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/orange-blue.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/gesautarquia.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/gesfaturacao.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/jquery.ml-keyboard.css') }}">
+    <!-- End Stylesheets -->
 
-    <link type="text/css" rel="stylesheet" href="css/orange-blue.css" >
-    <link type="text/css" rel="stylesheet" href="css/gesautarquia.css" >
-    <link type="text/css" rel="stylesheet" href="/css/gesfaturacao.css" >
-
-    <link rel="stylesheet" type="text/css" href="css/jquery.ml-keyboard.css"/>
     <!-- Add your custom styles here -->
-</head>
 
+</head>
 <body id="signin-page">
     <div class="logo-div" align="center">
         <a href="/">
@@ -40,8 +41,9 @@
             </div>
             <div class="body-content">
                 <div class="ptxl pbm" align="center">
-                    <img class="img-responsive" src="/uploads/faturacao/logo_default.png" alt="GESFaturação" style="max-height: 100px; margin-top:0px;"/>
-                </div>                <div class="pbxl" align="center"> </div>
+                    <img class="img-responsive" src="{{ asset('uploads/faturacao/logo_default.png') }}" alt="GESFaturação" style="max-height: 100px; margin-top:0px;"/>
+                </div>
+                <div class="pbxl" align="center"></div>
 
                 <!-- Email Address -->
                 <div class="form-group">
@@ -59,27 +61,13 @@
                     </div>
                 </div>
 
-                <!-- Remember Me -->
-                <div class="block mt-4">
-                    <label for="remember_me" class="inline-flex items-center">
-                        <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                        <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-                    </label>
-                </div>
-
                 <div class="text-center ptl pbl">
-                    <button type="submit" class="btn btn-grey submit_button" >
+                    <button type="submit" class="btn btn-grey submit_button">
                         <i class="fa fa-sign-in"></i> Entrar
                     </button>
                 </div>
 
-                <div class="text-center pbl">
-                    @if (Route::has('password.request'))
-                        <a href="{{ route('password.request') }}" class="options-link" ><i class="fa fa-key"></i> Recuperar Password</a>
-                    @endif
-                </div>
-
-                <div class="text-center pbl"> </div>
+                <div class="text-center pbl"></div>
             </div>
         </form>
     </div>
@@ -89,61 +77,15 @@
         </a>
     </div>
     <p class="text-center">
-        <b >GESImóveis desenvolvido por<a href="https://www.ftkode.com" class="options-link" target="_blank">FTKode Software</a>© {{ date("Y") }} | Todos os direitos reservados. </b>
+        <b>GESImóveis desenvolvido por <a href="https://www.ftkode.com" class="options-link" target="_blank">FTKode Software</a>© {{ date("Y") }} | Todos os direitos reservados.</b>
     </p>
+
+    <!-- JavaScripts -->
+    <script src="{{ asset('js/jquery-1.10.2.min.js') }}"></script>
+    <script src="{{ asset('js/jquery-migrate-1.2.1.min.js') }}"></script>
+    <script src="{{ asset('js/jquery-ui.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.ml-keyboard.min.js') }}"></script>
+    <!-- End JavaScripts -->
 </body>
-
-<script src="js/jquery-1.10.2.min.js"></script>
-<script src="js/jquery-migrate-1.2.1.min.js"></script>
-<script src="js/jquery-ui.js"></script>
-<!--loading bootstrap js-->
-<script src="js/bootstrap.min.js"></script>
-
-<script type="text/javascript" src="/js/jquery.ml-keyboard.min.js"></script>
-
-<script>
-    let keyboardActive = false;
-
-    $('#keyboard_login').click(function(){
-        if(keyboardActive){
-            keyboardActive = false
-            setCookie('vkeyboard_login', keyboardActive)
-            $(this).html('<i class="fa fa-keyboard-o"></i> Ativar Teclado Virtual')
-        }else{
-            keyboardActive = true
-            setCookie('vkeyboard_login', keyboardActive)
-            $(this).html('<i class="fa fa-keyboard-o"></i> Desativar Teclado Virtual')
-        }
-    });
-
-    function closeKeyboard() {
-        $('#mlkeyboard').removeClass('force-open');
-        $('#mlkeyboard').hide();
-    }
-
-    function setCookie(cname, cvalue) {
-        let tenYearsFromNow = new Date();
-        tenYearsFromNow.setFullYear(tenYearsFromNow.getFullYear() + 10);
-        document.cookie = cname + "=" + cvalue + ";expires=" + tenYearsFromNow + "; path=/;";
-    }
-
-    $(document).ready(function(e) {
-        let vkeyboardLogin = getCookie('vkeyboard_login')
-        keyboardActive = vkeyboardLogin == '' || vkeyboardLogin == 'false'   ? false : true
-        if(keyboardActive){
-            $('#keyboard_login').html('<i class="fa fa-keyboard-o"></i> Desativar Teclado Virtual')
-        }else{
-            $('#keyboard_login').html('<i class="fa fa-keyboard-o"></i> Ativar Teclado Virtual')
-        }
-
-        $('#username,#password').mlKeyboard({layout: 'pt_PT',active_shift: false});
-
-        let closeKeyboardBtn = '<div><div class="keyboard-corner">' +
-            '<div class="circCont">' +
-            '<button onclick="closeKeyboard()" class="circle" data-animation="simpleRotate" data-remove="200"></button></div></div>'
-
-        $('#mlkeyboard ul').append(closeKeyboardBtn);
-    });
-</script>
-
 </html>
